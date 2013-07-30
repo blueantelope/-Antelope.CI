33020c7ed69dd3786751901a300553b73c1db3bb
blueantelope <blueantelope@gmail.com>
2013-07-30 23:38:25 +0800

    增加bus server工程

---
+++ com.antelope.ci.bus/com.antelope.ci.bus.server/src/main/java/com/antelope/ci/bus/server/MainServer.java
@@ -0,0 +1,40 @@
+// com.antelope.ci.bus.server.MainServer.java
+/**
+ * Antelope CI平台，持续集成平台
+ * 支持分布式部署测试，支持基于工程、任务多种集成模式
+ * ------------------------------------------------------------------------
+ * Copyright (c) 2013, Antelope CI Team All Rights Reserved.
+*/
+
+package com.antelope.ci.bus.server;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+
+
+/**
+ * TODO 描述
+ *
+ * @author   blueantelope
+ * @version  0.1
+ * @Date	 2013-7-30		下午11:23:33 
+ */
+public class MainServer implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
---
+++ com.antelope.ci.bus/com.antelope.ci.bus.server/src/test/java/com/antelope/ci/bus/server/test/TestServer.java
@@ -0,0 +1,22 @@
+// com.antelope.ci.bus.server.test.TestServer.java
+/**
+ * Antelope CI平台，持续集成平台
+ * 支持分布式部署测试，支持基于工程、任务多种集成模式
+ * ------------------------------------------------------------------------
+ * Copyright (c) 2013, Antelope CI Team All Rights Reserved.
+*/
+
+package com.antelope.ci.bus.server.test;
+
+
+/**
+ * TODO 描述
+ *
+ * @author   blueantelope
+ * @version  0.1
+ * @Date	 2013-7-30		下午11:24:39 
+ */
+public class TestServer {
+
+}
+
