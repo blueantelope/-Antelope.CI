faff075e58730d6e6f912a1747763ba6c9622666
blueantelope <blueantelope@gmail.com>
2013-07-31 10:16:47 +0800

    增加ci bus的主程序

---
+++ com.antelope.ci.bus/com.antelope.ci.bus/src/main/java/com/antelope/ci/bus/CIBus.java
@@ -0,0 +1,22 @@
+// com.antelope.ci.bus.CIBus.java
+/**
+ * Antelope CI平台，持续集成平台
+ * 支持分布式部署测试，支持基于工程、任务多种集成模式
+ * ------------------------------------------------------------------------
+ * Copyright (c) 2013, Antelope CI Team All Rights Reserved.
+*/
+
+package com.antelope.ci.bus;
+
+
+/**
+ * CI BUS的程序入口
+ *
+ * @author   blueantelope
+ * @version  0.1
+ * @Date	 2013-7-31		上午10:12:15 
+ */
+public class CIBus {
+
+}
+
