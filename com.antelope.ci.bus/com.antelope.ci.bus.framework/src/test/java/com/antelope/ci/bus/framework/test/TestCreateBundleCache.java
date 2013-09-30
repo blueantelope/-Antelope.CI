703b55160ea85db2861a7ec8bc88f2576e70af99
blueantelope <blueantelope@gmail.com>
2013-09-30 17:48:41 +0800

    Test unit for framework

--- com.antelope.ci.bus/com.antelope.ci.bus.framework/src/test/java/com/antelope/ci/bus/framework/test/TestCreateBundleCache.java
+++ com.antelope.ci.bus/com.antelope.ci.bus.framework/src/test/java/com/antelope/ci/bus/framework/test/TestCreateBundleCache.java
@@ -8,17 +8,7 @@
 
 package com.antelope.ci.bus.framework.test;
 
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
-import junit.framework.TestCase;
-
-import org.apache.felix.framework.Logger;
-import org.apache.felix.framework.cache.BundleCache;
-import org.junit.Before;
 import org.junit.Test;
-import org.osgi.framework.Constants;
 
 
 /**
@@ -27,23 +17,11 @@ import org.osgi.framework.Constants;
  * @version  0.1
  * @Date	 2013-9-29		下午9:21:21 
  */
-public class TestCreateBundleCache extends TestCase {
-	private BundleCache m_cache;
-	private String location;
-	
-	@Before
-	protected void setUp() throws Exception {
-		Logger logger = new Logger(); 
-		Map configMap = new HashMap();
-		URL root_url = TestCreateBundleCache.class.getResource("/");
-		configMap.put(Constants.FRAMEWORK_STORAGE, root_url.getFile());
-		location = TestCreateBundleCache.class.getResource("test.jar").toURI().toString();
-		m_cache = new BundleCache(logger, configMap);
-	}
-	
+public class TestCreateBundleCache extends TestBase {
 	@Test
 	public void test() throws Exception {
-		m_cache.create(1, 1, location, null);
+		m_archive = m_cache.create(1, 1, location, null);
+		m_archive.closeAndDelete();
 	}
 	
 	public static void main(String[] args) {
