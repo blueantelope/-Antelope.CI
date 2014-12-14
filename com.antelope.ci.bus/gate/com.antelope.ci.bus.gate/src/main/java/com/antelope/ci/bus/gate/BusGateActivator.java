fd7f3cf94516baa8bccde5a30ee1480671576d44
blueantelope <blueantelope@gmail.com>
2014-12-14 22:19:12 +0800

    add gate for ci

---
+++ com.antelope.ci.bus/gate/com.antelope.ci.bus.gate/src/main/java/com/antelope/ci/bus/gate/BusGateActivator.java
@@ -0,0 +1,70 @@
+// com.antelope.ci.bus.gate.BusGateActivator.java
+/**
+ * Antelope CI平台，持续集成平台
+ * 支持分布式部署测试，支持基于工程、任务多种集成模式
+ * ------------------------------------------------------------------------
+ * Copyright (c) 2014, Antelope CI Team All Rights Reserved.
+*/
+
+package com.antelope.ci.bus.gate;
+
+import org.osgi.framework.ServiceReference;
+
+import com.antelope.ci.bus.common.exception.CIBusException;
+import com.antelope.ci.bus.server.BusCommonServerActivator;
+
+
+/**
+ *
+ * @author   blueantelope
+ * @version  0.1
+ * @Date	 2014年12月13日		下午8:00:59 
+ */
+public class BusGateActivator extends BusCommonServerActivator {
+
+	@Override
+	protected void run() throws CIBusException {
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void handleLoadService(String clsName, ServiceReference ref,
+			Object service) throws CIBusException {
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void handleUnloadService(ServiceReference ref)
+			throws CIBusException {
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void handleStopAllService() throws CIBusException {
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void addServices() throws CIBusException {
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void removeServices() throws CIBusException {
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
